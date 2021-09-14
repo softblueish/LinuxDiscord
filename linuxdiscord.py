20ae47a1a5382c57c4f54bb4519e5cbb8b8602c6
Rebecca White <transsentientbeing@outlook.com>
2021-09-14 11:32:30 +0100

    Piss.
    I added it to the wrong FUCKING BRANCH

--- linuxdiscord.py
+++ linuxdiscord.py
@@ -1,7 +1,5 @@
-# bot.py
-import os
-import subprocess
-
+#!/usr/bin/python3
+import os, subprocess, rules
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -12,8 +10,14 @@ bot = commands.Bot(command_prefix='$')
 
 @bot.command(name='bash')
 async def bash(ctx, command):
-    
+    for i in range(len(rules.BLACKLIST_COMMANDS)):
+        if command == rules.BLACKLIST_COMMANDS[i]:
+            await ctx.send(rules.BLST_ERROR)
+            print(f"Blacklisted Command Attempted: {command}")
+            return
+
     rawstring = str(subprocess.check_output(command, shell=True), 'utf8').split('\\n')
+    print(f"Command Used: {command}")
     for i in range(len(rawstring)):
         await ctx.send(f"```{rawstring[i]}```")
 
---
+++ rules.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Commands that you don't want to be used
+BLACKLIST_COMMANDS=["sudo","su","shutdown","reboot"]
+
+# Error Messages
+BLST_ERROR="> This is a blacklisted command, and as such cannot be used."
